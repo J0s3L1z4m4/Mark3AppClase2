90cc4cca798a223792ae1e918693e7831c23fedc
Jose Lizama <jose.lizama1@kyndryl.com>
2022-08-27 02:45:06 -0400

    Creando Nav Bar

---
+++ appclase2/src/components/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import logo from '../../logo.svg';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <div className='navbar-container'>
+            <div>
+                <img src= {logo} alt="logo" />
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacto</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </div>
+    );
+
+};
+
+export default NavBar;
